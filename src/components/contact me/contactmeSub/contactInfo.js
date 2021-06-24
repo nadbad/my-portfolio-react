7feedb65372c273b7d7a97d32d21dc5db4686287
Nadeem <nadbadvlogs@gmail.com>
2021-06-24 13:36:40 +0200

    edited contact section and navbar label

--- src/components/contact me/contactmeSub/contactInfo.js
+++ src/components/contact me/contactmeSub/contactInfo.js
@@ -10,35 +10,16 @@ export default function ContactInfo() {
 				<a href='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nadeem.develops@gmail.com'>
 					<img src={Gmail} alt='email me' />
 				</a>
-				<p>
-					<a
-						href='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nadeem.develops@gmail.com'
-						className='fancy-link'>
-						nadeem.develops@gmail
-					</a>
-				</p>
 			</div>
 			<div className='section-2-contact' data-aos='fade-up'>
 				<a href='https://www.linkedin.com/in/nadeem-alhassan/'>
 					<img src={Linkedin} alt='my linkedin' />
 				</a>
-				<p>
-					<a
-						href='https://www.linkedin.com/in/nadeem-alhassan/'
-						className='fancy-link'>
-						linkedin.com/in/nadeem-alhassan
-					</a>
-				</p>
 			</div>
 			<div className='section-3-contact' data-aos='fade-up'>
 				<a href='https://github.com/nadbad'>
 					<img src={Github} alt='my github' />
 				</a>
-				<p>
-					<a href='https://github.com/nadbad' className='fancy-link'>
-						github.com/nadbad
-					</a>
-				</p>
 			</div>
 		</div>
 	);
